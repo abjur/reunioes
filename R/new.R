b122c80d05f755c09eb47d4f47272827e76d2ee3
Julio Trecenti <julio.trecenti@gmail.com>
2017-01-17 19:08:56 -0200

    add dir param and interactive file edit

--- R/new.R
+++ R/new.R
@@ -3,14 +3,22 @@
 #' Abrir template de reunioes.
 #'
 #' @param nome Titulo da reuniao.
+#' @param dir Pasta onde os arquivos serão salvos.
 #' @param data data em formato YYYYMMDD. Default data atual.
 #'
 #' @export
-new_meet <- function(nome, data = format(Sys.Date(), '%Y%m%d')) {
+new_meet <- function(nome, dir = 'reunioes',
+                     data = format(Sys.Date(), '%Y%m%d')) {
+  dir.create(dir, recursive = TRUE, showWarnings = FALSE)
   f <- system.file('template.Rmd', package = 'reunioes')
   data <- format(lubridate::ymd(data), '%Y%m%d')
-  fout <- sprintf('%s_%s.Rmd', data, gsub('[^a-z0-9]+', '_', tolower(nome)))
+  nm <- gsub('[^a-z0-9]+', '_', tolower(nome))
+  fout <- sprintf('%s/%s_%s.Rmd', dir, data, nm)
   rmarkdown::render(f, rmarkdown::md_document('markdown'),
-                    output_file = fout, output_dir = './', quiet = TRUE)
-  utils::file.edit(fout)
+                    output_file = fout, output_dir = dir, quiet = TRUE)
+  if (interactive()) {
+    rstudioapi::navigateToFile(fout)
+  } else {
+    utils::file.edit(fout)
+  }
 }
